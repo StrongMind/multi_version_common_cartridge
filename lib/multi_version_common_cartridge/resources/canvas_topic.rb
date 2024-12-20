55344076dfdd022d0218e0c1bb21ded398c6ef59
Sami Tanquary <samantha.tanquary@strongmind.com>
2024-12-20 11:57:54 -0700

    (S) PWN-783: use base class for inheritance
    Co-authored-by: Paul Shippy <paul.shippy@strongmind.com>

--- lib/multi_version_common_cartridge/resources/canvas_topic.rb
+++ lib/multi_version_common_cartridge/resources/canvas_topic.rb
@@ -1,7 +1,7 @@
 module MultiVersionCommonCartridge
   module Resources
-    class CanvasTopic < MultiVersionCommonCartridge::Resources::Resource
-      attr_accessor :topic_id, :title, :assignment_group_identifierref, :points_possible,
+    class CanvasTopic < Topic
+      attr_accessor :assignment_group_identifierref, :points_possible,
                     :max_attempts, :allowed_attempts, :is_end_of_module_exam, :grading_type, :submission_types,
                     :peer_review_count, :position, :workflow_state, :discussion_type, :type
     end
