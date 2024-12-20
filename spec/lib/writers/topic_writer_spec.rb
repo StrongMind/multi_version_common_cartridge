6e7524ee8c3d2048e0cf31268928fe0cda4f9fe6
Sami Tanquary <samantha.tanquary@strongmind.com>
2024-12-20 11:59:13 -0700

    (T) PWN-783: fix specs to use updated method name and resource
    Co-authored-by: Paul Shippy <paul.shippy@strongmind.com>

--- spec/lib/writers/topic_writer_spec.rb
+++ spec/lib/writers/topic_writer_spec.rb
@@ -1,39 +1,40 @@
 describe MultiVersionCommonCartridge::Writers::TopicWriter do
-  describe '#write' do
-    subject(:write) { topic_writer.write(dir) }
+  describe '#create_files' do
+    subject(:create_files) { topic_writer.create_files(dir) }
 
-    let(:topic_writer) { described_class.new(topic, version) }
+    let(:topic_writer) { described_class.new(resource, version) }
     let(:version) { MultiVersionCommonCartridge::CartridgeVersions::CC_1_2_0 }
     let(:dir) { Dir.mktmpdir }
     let(:topic_element) { topic_writer.topic_element }
-    let(:xml_file) { Nokogiri.parse(File.read("#{dir}/topic.xml")) }
-    let(:topic) { MultiVersionCommonCartridge::Resources::Topic.new.tap { |t| t.title = title; t.text = text } }
+    let(:xml_file) { Nokogiri.parse(File.read("#{dir}/#{resource.identifier}.xml")) }
+    let(:resource) { MultiVersionCommonCartridge::Resources::Topic.new.tap { |r| r.title = title; r.text = text } }
     let(:title) { 'Some title' }
     let(:text) { 'Some text' }
+    let(:identifier) { 'some_identifier' }
 
     it 'has a root element called topic' do
-      write
+      create_files
       expect(xml_file.root.name).to eq('topic')
     end
 
     it 'has namespaces on the root element' do
-      write
+      create_files
       expect(xml_file.root.namespace.href).to eq('http://www.imsglobal.org/xsd/imsccv1p1/imsdt_v1p1')
       expect(xml_file.root.namespace_definitions.find { |ns| ns.prefix == 'xsi' }.href).to eq('http://www.w3.org/2001/XMLSchema-instance')
     end
 
     it 'has a title' do
-      write
+      create_files
       expect(xml_file.at_xpath('//*:topic/*:title').text).to eq(title)
     end
 
     it 'has a text' do
-      write
+      create_files
       expect(xml_file.at_xpath('//*:topic/*:text').text).to eq(text)
     end
 
     it 'sets the type attribute' do
-      write
+      create_files
       expect(xml_file.at_xpath('//*:topic/*:text').attributes['type'].value).to eq('text/html')
     end
 
@@ -45,7 +46,7 @@ describe MultiVersionCommonCartridge::Writers::TopicWriter do
       end
 
       it 'sets the required namespaces' do
-        write
+        create_files
         expect(topic_element.xmlns_xsi).to eq(required_namespaces['xmlns:xsi'])
         expect(topic_element.xmlns).to eq(required_namespaces['xmlns:imsdt'])
       end
@@ -59,7 +60,7 @@ describe MultiVersionCommonCartridge::Writers::TopicWriter do
       end
 
       it 'sets the required namespaces' do
-        write
+        create_files
         expect(topic_element.xmlns_xsi).to eq(required_namespaces['xmlns:xsi'])
         expect(topic_element.xmlns).to eq(required_namespaces['xmlns:imsdt'])
       end
