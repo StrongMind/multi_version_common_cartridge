d170ebd90dfdbf1db0052648e759fc734541827e
Paul Shippy <paul.shippy@strongmind.com>
2024-12-20 08:53:35 -0700

    PWN-783: Remove redundant attribute

--- lib/multi_version_common_cartridge/resources/canvas_topic.rb
+++ lib/multi_version_common_cartridge/resources/canvas_topic.rb
@@ -1,8 +1,9 @@
 module MultiVersionCommonCartridge
   module Resources
     class CanvasTopic < MultiVersionCommonCartridge::Resources::Resource
-      attr_accessor :identifier, :topic_id, :title, :assignment_group_identifierref, :points_possible,
-                    :max_attempts, :allowed_attempts, :is_end_of_module_exam, :grading_type, :submission_types, :peer_review_count, :position, :workflow_state, :discussion_type, :type
+      attr_accessor :topic_id, :title, :assignment_group_identifierref, :points_possible,
+                    :max_attempts, :allowed_attempts, :is_end_of_module_exam, :grading_type, :submission_types,
+                    :peer_review_count, :position, :workflow_state, :discussion_type, :type
     end
   end
 end
